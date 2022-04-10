b2ffa26f44c73be43ac8ba146c2c017e849c5a77
Himanshi Rawat <himanshi@example.com>
2022-04-10 23:07:30 +0530

    module 5

--- examples/Week 3/js/script.js
+++ examples/Week 3/js/script.js
@@ -32,7 +32,7 @@ var insertHtml = function (selector, html) {
 // Show loading icon inside element identified by 'selector'.
 var showLoading = function (selector) {
   var html = "<div class='text-center'>";
-  html += "<img src='https://gfycat.com/understatedfrayedalabamamapturtle'></div>";
+  html += "<img src ='image/ajax-loader.gif '></div>";
   insertHtml(selector, html);
 };
 
